fbd554f0bb207487c2fd75f88bf946d40a956386
Eric Moynihan <git@moynihan.io>
2019-09-23 13:27:28 +0100

    Added firebase popup

--- frontend/src/queries/Account.ts
+++ frontend/src/queries/Account.ts
@@ -3,9 +3,21 @@ import 'firebase/auth'
 
 export default class Account {
   app: firebase.app.App
+  user?: firebase.User
 
   constructor(app: firebase.app.App) {
     this.app = app
+  }
+
+  async signIn() {
+    let result = await this.app.auth().signInWithPopup(
+     new firebase.auth.GoogleAuthProvider().setCustomParameters({
+       'login_hint': 'example@umail.ucc.ie'
+     })
+    )
+    let token = result.credential.toJSON()
+    let user = result.user
+
   }
 
 }
