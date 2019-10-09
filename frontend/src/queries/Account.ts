59ac5f21f9b3a64d38da639b4aa1e98d77200a02
Eric Moynihan <git@moynihan.io>
2019-10-09 14:31:02 +0100

    Refactored firebase

--- frontend/src/queries/Account.ts
+++ frontend/src/queries/Account.ts
@@ -2,34 +2,42 @@ import * as firebase from 'firebase/app'
 import 'firebase/auth'
 
 export default class Account {
-  private app: firebase.app.App
-  private user: firebase.User | null = null
+  private $app: firebase.app.App
+  private $user: firebase.User | null = null
 
   constructor(app: firebase.app.App) {
-    this.app = app
+    this.$app = app
   }
 
   async signIn() {
     try {
-      let result = await this.app.auth().signInWithPopup(
+      let result = await this.$app.auth().signInWithPopup(
         new firebase.auth.GoogleAuthProvider().setCustomParameters({
           login_hint: 'example@umail.ucc.ie'
         })
       )
-      this.user = result.user
+      this.handleUser(result.user)
     } catch (error) {
       console.error(error)
     }
   }
 
-  private handleUser(user: firebase.User) {}
+  private handleUser(user: firebase.User) {
+    const EMAIL_REG = /[0-9]+@umail.ucc.ie/
+    if (user.email.match(EMAIL_REG)) {
+      // Is a valid UCC email address
+      // Validation is also done on firebase but to display instant signin we do it here too.
+      // If regex were to be modified here, the user wouldnt exist in our DB and website would throw exception
+      this.$user = user
+    }
+  }
 
   getUser(): firebase.User {
     // Only call when checked login status
-    return this.user as firebase.User
+    return this.$user as firebase.User
   }
 
   loggedIn(): boolean {
-    return !!this.user
+    return !!this.$user
   }
 }
